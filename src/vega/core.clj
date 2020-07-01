6ffb860d902b4d7cac1855a081d1e43dea184b05
Tony Duan <tony84727@gmail.com>
2020-07-02 00:13:56 +0800

    yes! a http server

--- src/vega/core.clj
+++ src/vega/core.clj
@@ -1,7 +1,27 @@
 (ns vega.core
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server :as s]
+            [compojure.core :refer :all]
+            [compojure.route :as cr]))
+(defonce server (atom nil))
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)))
+(defn hello [req]
+  {:status 200
+   :body "Welcome to vega"})
+(defroutes all-routes
+  (GET "/" [] hello)
+  (cr/not-found {:status 404 :body "vega can't understand this :<"}))
+(defn start-server []
+  (s/run-server all-routes {:port 8080}))
+
+(defn restart-server []
+  (stop-server)
+  (start-server))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (start-server)
+  (println "vega http server started"))
