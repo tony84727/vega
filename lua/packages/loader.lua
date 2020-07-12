ecfb255a84ff20bc24b98930aa59c40f1c6a925a
Tony Duan <tony84727@gmail.com>
2020-07-12 16:42:45 +0800

    Implement loader (#4)
    * implement loader self-update
    
    * make loader able to load other packages
    
    * fix loader bugs
    
    deal with empty package files

--- lua/packages/loader.lua
+++ lua/packages/loader.lua
@@ -1,14 +1,40 @@
 local internet = require("internet")
 local filesystem = require("filesystem")
 local loaderPackageID = "loader"
-local vega = "http://localhost:8080/"
-local packageRepository = vega .. "packages/"
 
-function packageURL(repository, id)
-  return repository  .. id
+function loadVegaConfig()
+  if filesystem.exists("/home/vega.host") then
+    local file, err = filesystem.open("/home/vega.host")
+    if err ~= nil then
+      print("failed to load vega.host configuration")
+      return ""
+    end
+    host = file:read(1024)
+    file:close()
+    return host
+  end
+  return ""
 end
 
-function httpDownloadTo(url, path)
+local vega = "http://localhost:8080/"
+local configHost = loadVegaConfig()
+if string.len(configHost) > 0 then
+  vega = configHost
+end
+
+function packageURLWithQueryString(repository, id, queryString)
+  local url = repository  .. id
+  if string.len(queryString) > 0 then
+    url = url .. "?" .. queryString
+  end
+  return url
+end
+local packageRepository = vega .. "packages/"
+function packageURL(repository, id)
+  return packageURLWithQueryString(repository, id, '')
+end
+
+function httpGet(url)
   local handle = internet.request(url)
   local response = ""
   local it = nil
@@ -19,6 +45,11 @@ function httpDownloadTo(url, path)
     end
     response = response .. buffer
   end
+  return response
+end
+
+function httpDownloadTo(url, path)
+  response = httpGet(url)
   filesystem.makeDirectory(filesystem.path(path))
   local file, err = io.open(path, "w")
   if err ~= nil then
@@ -29,13 +60,64 @@ function httpDownloadTo(url, path)
 end
 
 function fetchPackage(package)
-  httpDownloadTo(packageURL(packageRepository, package), "/home/vega/packages/" .. package .. ".lua")
+  httpDownloadTo(packageURL(packageRepository, package), "/home/bin/" .. package .. ".lua")
 end
 
-function selfCheck(packageRepository)
-  print("starting self-update checking")
-  print("fetching" .. packageRepository)
-  local handle = internet.request()
+function getPackageMD5(id)
+  return httpGet(packageURLWithQueryString(packageRepository, id, "md5"))
 end
 
-fetchPackage("loader")
+function getChecksum(path)
+  local file, err = filesystem.open(path)
+  if err ~= nil then
+    print("unable to access " .. path .. " " .. err)
+    return ""
+  end
+  local header = file:read(50)
+  if header == nil then
+    return ""
+  end
+  file:close()
+  local checksumEnd = string.find(header, "]")
+  if checksumEnd == nil then
+    return ""
+  else
+    checksumEnd = checksumEnd - 1
+  end
+  return string.sub(header, 5, checksumEnd)
+end
+
+function checkPackage(package)
+  local packageFile = "/home/bin/" .. package .. ".lua"
+  local exists = filesystem.exists(packageFile)
+  if exists then
+    local remoteChecksum = getPackageMD5("loader")
+    local localChecksum = getChecksum(packageFile)
+    print("remote checksum: " .. remoteChecksum)
+    print("local checksum: " .. localChecksum)
+    if remoteChecksum == localChecksum then
+      print("update-to-date")
+      return
+    end
+    print("new version of " .. package .. " available, updating ... ")
+    -- backup
+    local success = filesystem.copy("/home/bin/" .. package .. ".lua", "/home/bin/" .. package .. ".old.lua")
+    if not success then
+      print("failed to backup original version of " .. package)
+      return
+    end
+  end
+  fetchPackage(package)
+  print("done")
+end
+
+function selfCheck()
+  checkPackage("loader")
+end
+
+selfCheck()
+
+local args = {...}
+if args[1] ~= nil then
+  checkPackage(args[1])
+end
--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :Url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [http-kit "2.3.0"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [com.taoensso/timbre "4.10.0"]]
   :main ^:skip-aot vega.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
