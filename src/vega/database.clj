6df4b8119c229e72f6d4c30c668db20651bd29be
Tony Duan <tony84727@gmail.com>
2020-07-23 20:23:14 +0800

    fix create-migration-table-query

--- src/vega/database.clj
+++ src/vega/database.clj
@@ -12,9 +12,11 @@
                       session
                       (format "CREATE KEYSPACE %s WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1}" keyspace)))
 (defn create-migration-table-query [keyspace]
-  (format "CREATE TABLE IF NOT EXISTS %s.ragtime-migrations (id varchar, created_at timestamp, PRIMARY KEY (id))" keyspace))
-(defn ensure-migration-table! [session keyspace] (alia/execute session
-                                                              (create-migration-table-query keyspace)))
+  (format "CREATE TABLE IF NOT EXISTS %s.ragtime_migrations (id varchar, created_at timestamp, PRIMARY KEY (id))" keyspace))
+(defn ensure-migration-table! [session keyspace]
+  (alia/execute session
+                (create-migration-table-query keyspace)))
+
 (defrecord CassandraDataStore [session keyspace-name]
   rgp/DataStore
   (add-migration-id [_ id]
