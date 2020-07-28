7a1faf385bf5fdd2b56381a217d251d80ef1f584
Tony Duan <tony84727@gmail.com>
2020-07-28 13:15:39 +0800

    successfully connect the bot.
    but the go-loop and try-cache might has problem

--- src/vega/discord.clj
+++ src/vega/discord.clj
@@ -1,7 +1,42 @@
 (ns vega.discord
-  (:require [clj-yaml.core :as yaml]))
+  (:require [clj-yaml.core :as yaml]
+            [clojure.core.async :as a]
+            [discljord.connections :as c]
+            [discljord.messaging :as m]))
 
 (defn get-token []
   (-> (yaml/parse-string (slurp "config.yml"))
       (:discord)
       (:token)))
+
+(defn spawn-bot
+  "spawn and start a bot and return a stop channel"
+  [token]
+  (let [shutdown-ch (a/chan)
+        event-ch (a/chan)
+        connection-ch (c/connect-bot! token event-ch)
+        message-ch (m/start-connection! token)]
+    (try (a/go-loop []
+           (a/alt! 
+             [event-ch] ([[event-type event-data]]
+                 (when (= :channel-pins-update event-type)
+                   (c/disconnect-bot! connection-ch))
+                 (when-not (= :disconnect event-type)
+                   (recur)))
+             [shutdown-ch] ([] (c/disconnect-bot! connection-ch))))
+         (finally
+           (m/stop-connection! message-ch)
+           (a/close! event-ch)))
+    shutdown-ch))
+
+(defonce bot (atom nil))
+(defn start [token]
+  (swap! bot (fn [previous]
+                        (when-not (nil? previous) (a/close! previous))
+               (spawn-bot token))))
+
+(defn stop []
+  (swap! bot
+         (fn [previous ]
+           (when-not (nil? previous) (a/close! previous))
+           nil)))
