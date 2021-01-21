5bf9731f464432d3eafff9f9588d0a431fb525f9
Tony Duan <tony84727@gmail.com>
2021-01-21 22:48:27 +0800

    implemented the discord bot

--- src/vega/core.clj
+++ src/vega/core.clj
@@ -7,6 +7,7 @@
             [vega.packages :as packages]
             [vega.controlpanel.debugging :as debugging]
             [vega.messages :as messages]
+            [vega.discord :as discord]
             [ring.util.request :refer [body-string]]))
 (defonce server (atom nil))
 
@@ -46,5 +47,7 @@
   "I don't do a whole lot ... yet."
   [& args]
   (start-server)
-  (println "vega http server started"))
+  (println "vega http server started")
+  (discord/start-bot! (first @messages-broker))
+  (println "discord bot online"))
 
--- src/vega/discord.clj
+++ src/vega/discord.clj
@@ -4,35 +4,52 @@
             [discljord.messaging :as m]))
 (def ^:private default-config-path "discord.edn")
 
+(defonce bot (atom nil))
+(defonce messaging-ch (atom nil))
+
 (defn load-bot-config!
   []
   (read-string (slurp default-config-path)))
 
-(defn command-handlers
-  [messaging-ch commands]
-  (map (fn [[command message]]
-         (fn [args]
-           (async/go (async/>! messaging-ch message))))
-       commands))
+(def get-config!
+  (memoize (fn [] (load-bot-config!))))
 
-(defn- handle-message
-  [message-ch event-data]
-  (let [{channel-id :channel-id} event-data]
-    (m/create-message! message-ch channel-id :content (str event-data))))
+(defn command-handlers
+  [commands messaging-ch]
+  (into {} (map (fn [[command message]]
+                  [command (fn []
+                             (async/go (async/>! messaging-ch message)))])
+                commands)))
+(defn- parse-command
+  [message]
+  (second (re-matches #"!(\S+)" message)))
+
+(defn- handle-message-commands
+  [handlers event-data]
+  (let [{channel-id :channel-id content :content} event-data
+        command (parse-command content)
+        handler (when command (get handlers command))]
+    (when handler
+      (handler))))
+(defn- create-message-command-handler
+  [commands messaging-ch]
+  (let [handlers (command-handlers commands messaging-ch)]
+    (partial handle-message-commands handlers)))
 
 (defn connect-bot!
-  [token]
+  [token commands messaging-ch]
   (let [event-ch (async/chan 100)
         connection-ch (c/connect-bot! token event-ch :intents [:guilds :guild-messages])
         message-ch (m/start-connection! token)
-        stop (fn [] (c/disconnect-bot! connection-ch))]
+        stop (fn [] (c/disconnect-bot! connection-ch))
+        message-handler (create-message-command-handler commands messaging-ch)]
     (async/go
       (try
         (loop []
           (let [[event-type event-data] (async/<! event-ch)]
             (when-not (:bot (:author event-data))
               (when (= :message-create event-type)
-                (handle-message message-ch event-data)))
+                (message-handler event-data)))
             (when-not (= :disconnect event-type)
               (recur))))
         (finally
@@ -41,6 +58,7 @@
     stop))
 
 (defn start-bot!
-  []
+  [message-ch]
   (let [token (:token (load-bot-config!))]
-    (connect-bot! token)))
+    (reset! messaging-ch message-ch)
+    (swap! bot (fn [stop] (when stop (stop)) (connect-bot! token (:message-commands (get-config!)) message-ch)))))
