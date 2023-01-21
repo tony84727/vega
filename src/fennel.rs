a280231c5eb1e7b6403364df65a3d07697839c4f
Tony Duan <tony84727@gmail.com>
2023-01-21 22:32:09 +0800

    feat: fennel compiling

---
+++ src/fennel.rs
@@ -0,0 +1,56 @@
+use std::{
+    io::{self, Write},
+    process::{Command, Stdio},
+};
+
+use crossbeam::{channel::unbounded, select};
+
+#[derive(Debug)]
+pub enum CompileError {
+    IO(io::Error),
+    FailToOpenStdIn,
+    WriteStdIn,
+}
+
+pub fn compile(source: Vec<u8>) -> Result<Vec<u8>, CompileError> {
+    let mut compile = Command::new("fennel")
+        .args(&["-c", "-"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .map_err(CompileError::IO)?;
+    let (stdin_sender, stdin_receiver) = unbounded();
+    let (output_sender, output_receiver) = unbounded();
+    let mut stdin = compile.stdin.take().ok_or(CompileError::FailToOpenStdIn)?;
+    let stdin_write =
+        std::thread::spawn(move || stdin_sender.send(stdin.write_all(&source)).unwrap());
+    let output =
+        std::thread::spawn(move || output_sender.send(compile.wait_with_output().unwrap().stdout));
+    loop {
+        select! {
+            recv(stdin_receiver) -> result => match result {
+                Ok(result) => result.map_err(CompileError::IO)?,
+                Err(_err) => (),
+            },
+            recv(output_receiver) -> result => {
+                stdin_write.join().unwrap();
+                output.join().unwrap().unwrap();
+                return Ok(result.unwrap());
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::compile;
+
+    #[test]
+    fn test_compile() {
+        assert_eq!(
+            "local message = \"hello world\"\nreturn nil\n".to_string(),
+            String::from_utf8(compile(Vec::from("(local message \"hello world\")")).unwrap())
+                .unwrap()
+        )
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod fennel;
 pub mod package;
 #[cfg(test)]
 pub mod test_utils;
